963621ca03e0ea1bf8ff8f3e5d8224906e829897
AA13x-ctrl <zarif16@gmail.com>
2025-10-22 09:36:35 -0700

    Commit Initial Files

---
+++ ScoreBoard/index.js
@@ -0,0 +1,41 @@
+let card = document.getElementById("score-cd")
+
+console.log(card)
+
+let count = 0
+
+function incrementOneH() {
+  count = count + 1
+  card.innerText = count
+}
+
+function incrementTwoH() {
+  count = count + 2
+  card.innerText = count
+}
+
+function incrementThreeH() {
+  count = count + 3
+  card.innerText = count
+}
+
+let carda = document.getElementById("score-cda")
+
+console.log(carda)
+
+let countg = 0
+
+function incrementOneG() {
+  countg = countg + 1
+  carda.innerText = countg
+}
+
+function incrementTwoG() {
+  countg = countg + 2
+  carda.innerText = countg
+}
+
+function incrementThreeG() {
+  countg = countg + 3
+  carda.innerText = countg
+}
